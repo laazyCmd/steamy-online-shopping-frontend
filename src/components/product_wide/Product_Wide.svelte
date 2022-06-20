49d0b9f1764eecdc9f6d4ed173d4c7391b42f601
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-06-20 17:11:11 +0800

    added Product_Wide component

---
+++ src/components/product_wide/Product_Wide.svelte
@@ -0,0 +1,20 @@
+<section class="h-[277px] w-[486px] overflow-hidden rounded-sm">
+    <!-- Product Image -->
+    <header class="flex justify-center h-[60%] bg-white">
+        <img class="my-auto" src="/steamy-logo.png" alt="Steamy">
+    </header>
+    <!-- Product Details -->
+    <section class="flex flex-col bg-[#1A678A] h-[40%] p-4">
+        <!-- Name -->
+        <header class="h-full">
+            <span class="inline-block bg-white w-4/6 h-[23px] my-[3px] rounded-xl opacity-50"></span>
+        </header>
+        <!-- Price & Sales -->
+        <div class="flex justify-between items-center">
+            <p class="flex items-center bg-[#141F2C66] bg-opacity-40 px-3 py-[3px]">
+                <span class="inline-block bg-[#ACDBF5] w-[92px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+            </p>
+            <span class="inline-block bg-white w-[82px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+        </div>
+    </section>
+</section>
