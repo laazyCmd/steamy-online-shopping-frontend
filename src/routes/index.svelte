ab9b58095033b4be2f5e7f9a4cae80b4996386ca
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-06-20 13:45:38 +0800

    finished browse by steamy section, changed #products-popular -> a class

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -10,7 +10,7 @@
         <BrowseMore href="/search"/>
     </header>
     <!-- products -->
-    <div id="products-popular">
+    <div class="sections-content">
         <Product/>
         <Product/>
         <Product/>
@@ -22,18 +22,27 @@
     <header>
         <h1>BROWSE BY STEAMY</h1>
     </header>
-    <!-- products -->
-    <div id="products-popular">
-        <Product/>
-        <Product/>
-        <Product/>
+    <!-- Categories -->
+    <div class="sections-content">
+        <a class="categories-button" href="/">
+            LATEST
+        </a>
+        <a class="categories-button" href="/">
+            DISCOUNTED
+        </a>
+        <a class="categories-button" href="/">
+            FREE SHIPPING
+        </a>
+        <a class="categories-button" href="/">
+            FREEBIES
+        </a>
     </div>
 </section>
 
 <style>
     #index-sections {
         width: 100%;
-        margin-bottom: 3rem;
+        margin-bottom: 1rem;
     }
 
     #index-sections header {
@@ -42,10 +51,25 @@
         align-items: center;
     }
 
-    #products-popular {
+    .sections-content {
         display: flex;
         justify-content: space-between;
         margin-top: 10px;
         column-gap: 1rem;
+    }
+
+    .categories-button {
+        background-color: #10ABFF;
+        border-radius: 2px;
+        width: 100%;
+        text-align: center;
+        padding: 12px 1rem;
+        font-size: 1.125rem; /* 18px */
+        line-height: 1.75rem; /* 28px */
+        font-weight: 600;
+    }
+
+    .categories-button:hover {
+        background-color: #06bfff;
     }
 </style>
