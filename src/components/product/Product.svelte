ff1be0ea654b5f02914764266267337fcf9c9036
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-06-22 15:54:55 +0800

    added placeholder image, and no text will be loading by default

--- src/components/product/Product.svelte
+++ src/components/product/Product.svelte
@@ -1,17 +1,22 @@
-<a href="/" class="w-[290px] h-[415px] bg-[#1A678A] rounded-sm flex flex-col">
-    <!-- product image -->
-    <header class="h-full">
-        <figure>
-            <img src="/" alt="Product Image">
+<a href="/" class="w-[290px] h-[415px] bg-[#1A678A] rounded-sm overflow-hidden">
+    <!-- Product Image -->
+    <header class="flex justify-center h-[60%] bg-white">
+        <figure class="my-auto">
+            <img src="/steamy-logo.png" alt="Steamy" width="218" height="63">
         </figure>
     </header>
-    <!-- product details -->
-    <div class="flex flex-col h-[175px] px-3 py-3">
-        <p class="whitespace-nowrap overflow-hidden text-ellipsis h-full">Undefined dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
-        <!-- price & sales -->
-        <section class="flex justify-between">
-            <p class="bg-[#141F2C66] bg-opacity-40 px-3 py-[1px] text-[#ACDBF5]">P0000.00</p>
-            <p>0 sold</p>
-        </section>
-    </div>
+    <!-- Product Details -->
+    <section class="flex flex-col bg-[#1A678A] h-[40%] p-3">
+        <!-- Name -->
+        <header class="h-full">
+            <span class="inline-block bg-white w-4/6 h-[23px] my-[3px] rounded-xl opacity-50"></span>
+        </header>
+        <!-- Price & Sales -->
+        <div class="flex justify-between items-center">
+            <p class="flex items-center bg-[#141F2C66] bg-opacity-40 px-3 py-[3px]">
+                <span class="inline-block bg-[#ACDBF5] w-[92px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+            </p>
+            <span class="inline-block bg-white w-[82px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+        </div>
+    </section>
 </a>
