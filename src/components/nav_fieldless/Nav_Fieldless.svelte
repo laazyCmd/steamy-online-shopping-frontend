0bbcef9e0209df25a46db1838523f61eed19cf3b
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-06-26 15:50:35 +0800

    added a named layout that only removes the search field in nav

---
+++ src/components/nav_fieldless/Nav_Fieldless.svelte
@@ -0,0 +1,39 @@
+<nav class="relative bg-[#171A21] text-white h-[136px]">
+    <div class="flex justify-end gap-x-[18px] mx-auto max-w-[1307px] pt-[5px] text-opacity-25">
+        <section class="flex gap-x-[13px] items-center">
+            <!-- Help -->
+            <a class="flex items-center text-sm gap-x-[5px] hover:opacity-75" href="/help">
+                <!-- TODO: fix this icon -->
+                <figure>
+                    <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" width="18" height="18"><path d="M256 80a176 176 0 10176 176A176 176 0 00256 80z" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><path d="M200 202.29s.84-17.5 19.57-32.57C230.68 160.77 244 158.18 256 158c10.93-.14 20.69 1.67 26.53 4.45 10 4.76 29.47 16.38 29.47 41.09 0 26-17 37.81-36.37 50.8S251 281.43 251 296" fill="none" stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="28"/><circle cx="250" cy="348" r="20"/></svg>
+                </figure>
+                Help
+            </a>
+            <!-- Language -->
+            <button class="flex items-center text-sm gap-x-[5px] hover:opacity-75">
+                Language
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512" width="18" height="18"><path d="M98 190.06l139.78 163.12a24 24 0 0036.44 0L414 190.06c13.34-15.57 2.28-39.62-18.22-39.62h-279.6c-20.5 0-31.56 24.05-18.18 39.62z" fill="white"/></svg>
+            </button>
+        </section>
+        <section class="flex gap-x-[13px] items-center">
+            <!-- Login -->
+            <button class="text-sm font-semibold hover:opacity-75">
+                Login
+            </button>
+            <span>|</span>
+            <!-- Sign up -->
+            <button class="text-sm font-semibold hover:opacity-75">
+                Sign up
+            </button>
+        </section>
+    </div>
+    <header class="flex justify-around items-center mx-auto max-w-[1077px]">
+        <!-- navigation links/buttons -->
+        <section class="absolute bottom-0 flex gap-x-8 text-xl self-end mr-[53px] mb-[11px]">
+            <a class="hover:opacity-75" href="/">STORE</a>
+            <a class="hover:opacity-75" href="/search">NEW & POPULAR</a>
+            <button class="hover:opacity-75">CATEGORIES</button>
+            <a class="hover:opacity-75" href="/about">ABOUT</a>
+        </section>
+    </header>
+</nav>
---
+++ src/routes/__layout-fieldless.svelte
@@ -0,0 +1,11 @@
+<script>
+  import "../app.css";
+  import Nav_Fieldless from "$components/nav_fieldless/Nav_Fieldless.svelte";
+  import Footer from "$components/footer/Footer.svelte";
+</script>
+
+<Nav_Fieldless />
+<main class="flex flex-col flex-1 gap-y-16 mx-auto max-w-[904px] mt-[30px] mb-[80px] h-full w-full">
+  <slot/>
+</main>
+<Footer />
