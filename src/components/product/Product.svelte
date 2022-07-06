c760359db363893e3e9ed030e32447e697250688
laazyCmd <laazy.pr00gramming@protonmail.com>
2022-07-06 17:29:07 +0800

    product cards can now get product information

--- src/components/product/Product.svelte
+++ src/components/product/Product.svelte
@@ -1,22 +1,42 @@
-<a href="/" class="w-[290px] h-[415px] bg-[#1A678A] rounded-sm overflow-hidden">
+<script>
+    export let product = {};
+</script>
+
+<a href="/product/{ product.id }/{ encodeURIComponent( product.name ) }" class="w-[290px] h-[415px] bg-[#1A678A] rounded-sm overflow-hidden">
     <!-- Product Image -->
     <header class="flex justify-center h-[60%] bg-white">
         <figure class="my-auto">
-            <img src="/steamy-logo.png" alt="Steamy" width="218" height="63">
+            { #if product.image }
+                <img src="data:image/jpg;base64,{ product.image }" alt="Steamy" width="218" height="63">
+            { :else }
+                <img src="/steamy-logo.png" alt="Steamy" width="218" height="63">
+            { /if }
         </figure>
     </header>
     <!-- Product Details -->
     <section class="flex flex-col bg-[#1A678A] h-[40%] p-3">
         <!-- Name -->
         <header class="h-full">
-            <span class="inline-block bg-white w-4/6 h-[23px] my-[3px] rounded-xl opacity-50"></span>
+            { #if product.name }
+                <p>{ product.name }</p>
+            { :else }
+                <span class="inline-block bg-white w-4/6 h-[23px] my-[3px] rounded-xl opacity-50"></span>
+            { /if }
         </header>
         <!-- Price & Sales -->
         <div class="flex justify-between items-center">
             <p class="flex items-center bg-[#141F2C66] bg-opacity-40 px-3 py-[3px]">
-                <span class="inline-block bg-[#ACDBF5] w-[92px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+                { #if product.price }
+                    <p class="text-[#ACDBF5]">P{ product.price }</p>
+                { :else }
+                    <span class="inline-block bg-[#ACDBF5] w-[92px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+                { /if }
             </p>
-            <span class="inline-block bg-white w-[82px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+            { #if product.sales }
+                <p>{ product.sales } sold</p>
+            { :else }
+                <span class="inline-block bg-white w-[82px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+            { /if }
         </div>
     </section>
 </a>
--- src/components/product_wide/Product_Wide.svelte
+++ src/components/product_wide/Product_Wide.svelte
@@ -1,22 +1,46 @@
-<a href="/" class="h-[277px] w-[486px] overflow-hidden rounded-sm">
+<script>
+    export let product = {};
+</script>
+
+<a href="/{ "product/" + product.id }/{ encodeURIComponent( product.name ) }" class="h-[277px] w-[486px] overflow-hidden rounded-sm">
     <!-- Product Image -->
     <header class="flex justify-center h-[60%] bg-white">
         <figure class="my-auto">
-            <img src="/steamy-logo.png" alt="Steamy" width="221" height="64">
+            { #if product.image }
+                <img src="data:image/jpg;base64,{ product.image }" alt="Steamy" width="221" height="64">
+            { :else }
+                <img src="/steamy-logo.png" alt="Steamy" width="221" height="64">
+            { /if }
         </figure>
     </header>
     <!-- Product Details -->
     <section class="flex flex-col bg-[#1A678A] h-[40%] p-3">
         <!-- Name -->
         <header class="h-full">
-            <span class="inline-block bg-white w-4/6 h-[23px] my-[3px] rounded-xl opacity-50"></span>
+            { #if product.name }
+                <p>{ product.name }</p>
+            { :else }
+                <span class="inline-block bg-white w-4/6 h-[23px] my-[3px] rounded-xl opacity-50"></span>
+            { /if }
         </header>
         <!-- Price & Sales -->
         <div class="flex justify-between items-center">
             <p class="flex items-center bg-[#141F2C66] bg-opacity-40 px-3 py-[3px]">
-                <span class="inline-block bg-[#ACDBF5] w-[92px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+                { #if product.price || product.price === 0 }
+                    { #if product.price === 0 }
+                        <p class="text-[#ACDBF5]">FREEBIES</p>
+                    { :else }
+                        <p class="text-[#ACDBF5]">P{ product.price }</p>
+                    { /if }
+                { :else }
+                    <span class="inline-block bg-[#ACDBF5] w-[92px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+                { /if }
             </p>
-            <span class="inline-block bg-white w-[82px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+            { #if product.sales }
+                <p>{ product.sales } sold</p>
+            { :else }
+                <span class="inline-block bg-white w-[82px] h-[18px] opacity-50 rounded-xl my-[3px]"></span>
+            { /if }
         </div>
     </section>
 </a>
